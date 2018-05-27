9738c659fca6747fc6df14111b358c3511913704
apoc64 <sschwedt@gmail.com>
2018-05-26 22:34:29 -0600

    visitor can log in and see dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'bike_shop#index'
 
   get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'users#show'
 
