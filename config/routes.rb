e255e11ac9d6f8690d0b0f2a2d4ac72722ed5996
Eliot Swank <eliotswank@gmail.com>
2018-05-27 14:32:31 -0600

    fixes rebase issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   root to: 'bike_shop#index'
   get '/bike-shop', to: 'accessories#index'
 
@@ -12,7 +11,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-=======
->>>>>>> adds stations routes
   resources :stations, only: [:index, :show]
 end
