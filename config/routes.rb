05c05ff56d77eb06dcaa1c7350fcc70425967107
Eliot Swank <eliotswank@gmail.com>
2018-05-27 11:08:11 -0600

    adds stations routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
   root to: 'bike_shop#index'
   get '/bike-shop', to: 'accessories#index'
 
@@ -11,5 +12,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+=======
+>>>>>>> adds stations routes
   resources :stations, only: [:index, :show]
 end
