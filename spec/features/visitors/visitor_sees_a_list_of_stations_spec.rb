fbd94741f52728dcb022cafb84edb73b4a22d94f
Eliot Swank <eliotswank@gmail.com>
2018-05-27 11:46:01 -0600

    adds folder for visitor feature specs & test for visitor station show

--- spec/features/visitors/visitor_sees_a_list_of_stations_spec.rb
+++ spec/features/visitors/visitor_sees_a_list_of_stations_spec.rb
@@ -6,7 +6,7 @@ describe "A visitor" do
       station1 = Station.create(name: 'Foo', dock_count: 5, city: 'Denver', installation_date: Time.now)
       station2 = Station.create(name: 'Bar', dock_count: 4, city: 'Aurora', installation_date: Time.now)
 
-      visit '/stations'
+      visit station_path
 
 
       expect(page).to have_content(station1.name)
