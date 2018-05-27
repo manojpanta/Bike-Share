9206d662b973cbf131f6b224e50370ce647a347c
Eliot Swank <eliotswank@gmail.com>
2018-05-27 12:42:16 -0600

    works on changing tests to match association types

---
+++ spec/features/trips/visitor_sees_a_list_of_trips_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe "A visitor" do
+  context "visits the trips index page" do
+    it "and should see the start date, start station, end date, end station, bike id, subscription type, zip code" do
+      station = Station.create(name: 'Foo', dock_count: 5, city: 'Denver', installation_date: Time.now)
+      trip1 = Trip.create(start_date: Time.now, start_station: station.id, end_date: (Time.now + 1), end_station: station.id, bike_id: 4, subscription: 'Member', zip_code: 80202 )
+      trip2 = Trip.create(start_date: Time.now, start_station: station.id, end_date: (Time.now + 1), end_station: station.id, bike_id: 4, subscription: 'Member', zip_code: 80202 )
+
+      visit trips_path
+
+
+      expect(page).to have_content(trip1.start_date)
+      expect(page).to have_content(trip1.start_station)
+      expect(page).to have_content(trip1.end_date)
+      expect(page).to have_content(trip1.end_station)
+      expect(page).to have_content(trip1.bike_id)
+      expect(page).to have_content(trip1.subscription)
+      expect(page).to have_content(trip1.zip_code)
+      expect(page).to have_content(trip2.start_date)
+      expect(page).to have_content(trip2.start_station)
+      expect(page).to have_content(trip2.end_date)
+      expect(page).to have_content(trip2.end_station)
+      expect(page).to have_content(trip2.bike_id)
+      expect(page).to have_content(trip2.subscription)
+      expect(page).to have_content(trip2.zip_code)
+
+    end
+
+    it "and should see the top 30 trips with duration" do
+      
+    end
+    
+  end
+end
