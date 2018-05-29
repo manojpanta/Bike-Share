26457bd81813a68f904711448da9c7d1e9d9963c
apoc64 <sschwedt@gmail.com>
2018-05-29 14:12:41 -0600

    adds navbar functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,10 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
+      id = session[:user_id]
+      if User.exists?(id)
+        @current_user ||= User.find(id)
+      end
     end
   end
 
