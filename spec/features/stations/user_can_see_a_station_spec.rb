4976e7690ecb4389f0dc3bf38e7ee39ffefbbba0
Eliot Swank <eliotswank@gmail.com>
2018-05-30 16:03:54 -0600

    adds test for most frequent destination functionality

--- spec/features/stations/user_can_see_a_station_spec.rb
+++ spec/features/stations/user_can_see_a_station_spec.rb
@@ -37,6 +37,22 @@ describe "A registered user" do
 
       expect(page).to have_content("Rides ended here: 2")
     end
+
+    it "and sees the most frequent destination station" do
+      user = User.create(name: 'bob', email: 'bob@bob.bob', password: '1234', address: '123 Elm St', role: 0)
+      station = Station.create(name:'Foo', dock_count: 5, city: 'Denver', installation_date: Time.now)
+      station = Station.create(name:'Foo', dock_count: 5, city: 'Denver', installation_date: Time.now)
+      station = Station.create(name:'Foo', dock_count: 5, city: 'New Jack City', installation_date: Time.now)
+      Trip.create(duration: 100, start_date: Time.now, start_station: station, end_date: (Time.now + 1), end_station: station, bike_id: 4, subscription_type: 'Member', zip_code: 80202 )
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit station_path(station)
+
+      expect(page).to have_content("Most frequent destination: Denver")
+      expect(page).to_not have_content("Most frequent destination: New Jack City")
+    end
+
   end
 end
 
@@ -44,9 +60,7 @@ end
 
 # As a registered user,
 # When I visit a station show,
-# In addition to the user story above,
-# I see the Number of rides started at this station,
-# I see the Number of rides ended at this station,
+
 # I see the Most frequent destination station (for rides that began at this station),
 # I see the Most frequent origination station (for rides that ended at this station),
 # I see the Date with the highest number of trips started at this station,
