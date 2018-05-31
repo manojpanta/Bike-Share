59283637508a4c76a20c01b572dba9e686e7965b
Manoj Panta <manojpanta95@gmail.com>
2018-05-31 17:27:49 -0600

    mark as completed working

--- spec/features/orders/admin_can_change_status_of_paid_or_ordered_order_to_cancelled_spec.rb
+++ spec/features/orders/admin_can_change_status_of_paid_or_ordered_order_to_cancelled_spec.rb
@@ -53,4 +53,30 @@ describe 'admin visits admin dashboard' do
     expect(page).to have_content('paid')
     expect(page).to_not have_content('ordered')
   end
+  it 'can change status to completed from paid' do
+    user1 = User.create(name: 'bob', password: '1234', email: 'bob@bob.bob', address: '123 Elm St')
+
+    order1 = user1.orders.create(status: 'paid')
+
+    item1 = Accessory.create(title: 'chain', image: 'chain.jpg', price: 27.55, description: 'pedal to wheel')
+    item2 = Accessory.create(title: 'pedal', image: 'pedal.jpg', price: 36.55, description: 'goes around')
+
+    order1.accessory_orders.create(accessory: item1, quantity:2)
+    order1.accessories << item2
+
+    admin = User.create(name: 'admin', password: '1234', email: 'admin@bob.bob', address: '123 Elm St', role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit 'admin/dashboard'
+
+    expect(page).to have_content(order1.id)
+    expect(page).to have_content('paid')
+
+    within ('.orders-table') do
+      click_on 'Mark as Completed'
+    end
+
+    expect(page).to have_content('completed')
+    expect(page).to_not have_content('paid')
+  end
 end
