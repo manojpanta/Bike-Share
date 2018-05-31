ca3b123a49f9547c61ec39b0a6a2311d18f13d4a
Manoj Panta <manojpanta95@gmail.com>
2018-05-31 14:26:39 -0600

    wrote test for orders with completed status

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -82,5 +82,25 @@ RSpec.describe Order, type: :model do
 
       expect(Order.total_cancelled).to eq(1)
     end
+    it 'total no. of order with completed status' do
+      user1 = User.create(name: 'bob', password: '1234', email: 'bob@bob.bob', address: '123 Elm St')
+
+      order1 = user1.orders.create
+      order2 = user1.orders.create(status: 'paid')
+      order2 = user1.orders.create(status: 'cancelled')
+      order2 = user1.orders.create(status: 'completed')
+
+      item1 = Accessory.create(title: 'chain', image: 'chain.jpg', price: 27.55, description: 'pedal to wheel')
+      item2 = Accessory.create(title: 'pedal', image: 'pedal.jpg', price: 36.55, description: 'goes around')
+      order1.accessory_orders.create(accessory: item1, quantity:2)
+
+      order1.accessories << item2
+
+      admin = User.create(name: 'admin', password: '1234', email: 'admin@bob.bob', address: '123 Elm St', role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+
+      expect(Order.total_cancelled).to eq(1)
+    end
   end
 end
