a91a36cf9330725b9ff0ea931da4dcaec35e7925
apoc64 <sschwedt@gmail.com>
2018-05-31 21:36:58 -0600

    trips dashboard had subscriber info

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -12,9 +12,21 @@ class TripsController < ApplicationController
     @average = Trip.average(:duration)
     @longest = Trip.where(duration: Trip.maximum(:duration)).first.id
     @shortest = Trip.where(duration: Trip.minimum(:duration)).first.id
-    @most_starts = Station.find(Trip.select('COUNT(id) AS trip_count, start_station_id').group(:start_station_id).order('trip_count DESC').first.start_station_id)
-    @fewest_starts = Station.find(Trip.select('COUNT(id) AS trip_count, end_station_id').group(:end_station_id).order('trip_count DESC').first.end_station_id)
-    # binding.pry
+    @most_starts = Trip.select('COUNT(id) AS trip_count, start_station_id').group(:start_station_id).order('trip_count DESC').first.start_station
+    @fewest_starts = Trip.select('COUNT(id) AS trip_count, end_station_id').group(:end_station_id).order('trip_count DESC').first.end_station
 
+    bikes = Trip.select('COUNT(id) AS bike_count, bike_id').group(:bike_id).order('bike_count DESC')
+    @most_ridden_bike = bikes.first.bike_id
+    @most_bike_rides = bikes.first.bike_count
+    @least_ridden_bike = bikes.last.bike_id
+    @fewest_bike_rides = bikes.last.bike_count
+
+    subscriptions = Trip.select('COUNT(id) AS sub_count, subscription_type').group(:subscription_type).order('sub_count DESC')
+    # binding.pry
+    total_count = Trip.count
+    @subscriber_count = subscriptions.where(subscription_type: 'Subscriber').first.sub_count
+    @subscriber_percent = (@subscriber_count * 100) / total_count
+    @customer_count = subscriptions.where(subscription_type: 'Customer').first.sub_count
+    @customer_percent = (@customer_count * 100) / total_count
   end
 end
--- spec/features/trips/user_visits_station_dashboard_spec.rb
+++ spec/features/trips/user_visits_station_dashboard_spec.rb
@@ -4,10 +4,11 @@ describe "A user" do
   it "visits the trips dashboard" do
     station1 = Station.create(name: 'Foo', dock_count: 5, city: 'Denver', installation_date: Time.now.gmtime)
     station2 = Station.create(name: 'Bar', dock_count: 10, city: 'Denver', installation_date: Time.now.gmtime)
-
-    trip1 = Trip.create!(duration: 5, start_date: Time.now.gmtime, start_station: station1, end_date: (Time.now + 5).gmtime, end_station: station2, bike_id: 4, subscription_type: 'Member', zip_code: 80202 )
-    trip2 = Trip.create!(duration: 10, start_date: Time.now.gmtime, start_station: station1, end_date: (Time.now + 10).gmtime, end_station: station2, bike_id: 4, subscription_type: 'Customer', zip_code: 80202 )
-    trip3 = Trip.create!(duration: 20, start_date: Time.now.gmtime, start_station: station1, end_date: (Time.now + 20).gmtime, end_station: station2, bike_id: 4, subscription_type: 'Member', zip_code: 80202 )
+    date1 = DateTime.strptime('8/29/13 16:18', '%m/%d/%y %H:%M')
+    date2 = DateTime.strptime('8/29/14 16:18', '%m/%d/%y %H:%M')
+    trip1 = Trip.create!(duration: 5, start_date: date1, start_station: station1, end_date: (Time.now + 5).gmtime, end_station: station2, bike_id: 3, subscription_type: 'Subscriber', zip_code: 80202 )
+    trip2 = Trip.create!(duration: 10, start_date: date1, start_station: station1, end_date: (Time.now + 10).gmtime, end_station: station2, bike_id: 4, subscription_type: 'Customer', zip_code: 80202 )
+    trip3 = Trip.create!(duration: 20, start_date: date2, start_station: station1, end_date: (Time.now + 20).gmtime, end_station: station2, bike_id: 4, subscription_type: 'Subscriber', zip_code: 80202 )
 
     visit 'trips-dashboard'
 
@@ -17,6 +18,10 @@ describe "A user" do
     expect(page).to have_content("Shortest Ride:\n#{trip1.id}")
     expect(page).to have_content("Station With Most Rides Started:\n#{station1.name}")
     expect(page).to have_content("Station With Most Rides Ended:\n#{station2.name}")
+    expect(page).to have_content("Most Ridden Bike:\n#{trip2.bike_id}, 2 rides")
+    expect(page).to have_content("Least Ridden Bike:\n#{trip1.bike_id}, 1 rides")
+    expect(page).to have_content("Subscribers:\n2, %66")
+    expect(page).to have_content("Customers:\n1, %33")
 
   end
 end
