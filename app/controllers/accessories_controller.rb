010c540be23d1acc083229bdb81bd46bd176ef46
Manoj Panta <manojpanta95@gmail.com>
2018-06-02 16:27:23 -0600

    adding cart implementation to every page

--- app/controllers/accessories_controller.rb
+++ app/controllers/accessories_controller.rb
@@ -1,6 +1,7 @@
 class AccessoriesController < ApplicationController
   def index
     @accessories = Accessory.all
+    @cart = Cart.new(session[:cart])
   end
 
   def show
---
+++ app/models/cart.rb
@@ -0,0 +1,5 @@
+class Cart < ApplicationRecord
+
+
+
+end
