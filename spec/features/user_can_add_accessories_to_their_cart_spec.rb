28f7212fccb3295228583ffbdfc4bfa82981dd32
Manoj Panta <manojpanta95@gmail.com>
2018-06-02 15:51:54 -0600

    added test for add to cart feature test

---
+++ spec/features/user_can_add_accessories_to_their_cart_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'as a visitor' do
+  describe 'when I visit /bike-shop' do
+    describe 'when I click on Add to cart' do
+      it 'adds a accessory to the cart' do
+        accessory = Accessory.create!(title: 'wranch', image: 'http://blog.zealousgood.com/wp-content/uploads/2013/05/tools.jpg', price: 100, description: 'this is tool')
+
+        visit bike_shop_path
+
+        click_on 'Add to cart'
+
+        expect(current_path).to eq(bike_shop_path)
+        expect(page).to have_content("You now have 1 #{accessory.title} in your cart.")
+      end
+    end
+  end
+end
