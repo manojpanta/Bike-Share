2d3e10f987b514671de52f43814c3af36e3dd17c
Manoj Panta <manojpanta95@gmail.com>
2018-06-02 16:21:05 -0600

    added cart: 0 in application.html.erb

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,11 @@
 class CartsController < ApplicationController
+  include ActionView::Helpers::TextHelper
 
   def create
     accessory = Accessory.find(params[:accessory_id])
-    flash[:notice] = "You now have 1 #{accessory.title} in your cart."
+    session[:cart] ||= Hash.new(0)
+    session[:cart][accessory.id.to_s] = session[:cart][accessory.id.to_s] + 1
+    flash[:notice] = "You now have #{pluralize(session[:cart][accessory.id.to_s], accessory.title)} in your cart."
     redirect_to '/bike-shop'
   end
 
--- spec/features/user_can_add_accessories_to_their_cart_spec.rb
+++ spec/features/user_can_add_accessories_to_their_cart_spec.rb
@@ -13,6 +13,31 @@ describe 'as a visitor' do
         expect(page).to have_content("You now have 1 #{accessory.title} in your cart.")
         expect(current_path).to eq('/bike-shop')
       end
+
+      scenario "the message correctly increments for multiple accessories" do
+        accessory = Accessory.create!(title: 'wranch', image: 'http://blog.zealousgood.com/wp-content/uploads/2013/05/tools.jpg', price: 100, description: 'this is tool')
+        visit '/bike-shop'
+
+        click_button "Add to cart"
+
+        expect(page).to have_content("You now have 1 #{accessory.title} in your cart.")
+
+        click_button "Add to cart"
+
+        expect(page).to have_content("You now have 2 #{accessory.title}es in your cart.")
+      end
+
+      scenario "the total number of accessories in the cart increments" do
+
+        accessory = Accessory.create!(title: 'wranch', image: 'http://blog.zealousgood.com/wp-content/uploads/2013/05/tools.jpg', price: 100, description: 'this is tool')
+        visit '/bike-shop'
+
+        expect(page).to have_content("Cart: 0")
+
+        click_button "Add to cart"
+
+        expect(page).to have_content("Cart: 1")
+      end
     end
   end
 end
