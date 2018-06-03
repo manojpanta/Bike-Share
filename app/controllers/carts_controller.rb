a6685038a855fc612ba67fb801a2053f5e646606
apoc64 <sschwedt@gmail.com>
2018-06-02 18:45:34 -0600

    cart can calculate total cost

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -13,9 +13,11 @@ class CartsController < ApplicationController
   end
 
   def show
-    cart = Cart.new(session[:cart]).contents
-    @cart_items = cart.keys.map do |key|
-      [Accessory.find(key.to_i), cart[key]]
+    cart = Cart.new(session[:cart])
+    contents = cart.contents
+    @cart_items = contents.keys.map do |key|
+      [Accessory.find(key.to_i), contents[key]]
     end
+    @total = cart.total_cost
   end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe Cart do
       expect(cart.total_count).to eq(5)
     end
   end
+
+  describe 'total_cost' do
+    it 'can calculate its total cost' do
+      item1 = Accessory.create(title: 'chain', image: 'chain.jpg', price: 27.55, description: 'pedal to wheel')
+      item2 = Accessory.create(title: 'pedal', image: 'pedal.jpg', price: 36.55, description: 'goes around')
+
+      cart = Cart.new({item1.id.to_s => 2, item2.id.to_s => 3})
+
+      total_cost = (item1.price * 2) + (item2.price * 3)
+
+      expect(cart.total_cost).to eq(total_cost)
+    end
+  end
 end
