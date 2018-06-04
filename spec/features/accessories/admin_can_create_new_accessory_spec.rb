c9cc6cc5a1b8182c47d308a3d9a31e969df1b33a
Eliot Swank <eliotswank@gmail.com>
2018-06-04 14:24:53 -0600

    adds test for path redirect in admin_bikeshop_new

--- spec/features/accessories/admin_can_create_new_accessory_spec.rb
+++ spec/features/accessories/admin_can_create_new_accessory_spec.rb
@@ -22,4 +22,23 @@ describe 'user visits bikeshop new page' do
     expect(page).to have_content(description)
     expect(page).to have_content(price)
   end
+
+  it "it renders a new page if not filled out properly" do
+    admin = User.create(name: 'bob', email: 'bob@bob.bob', password: '1234', address: '123 Elm St', role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit admin_bikeshop_new_path
+
+    price = 123
+    image = 'http://blog.zealousgood.com/wp-content/uploads/2013/05/tools.jpg'
+
+    fill_in 'accessory[title]', with: ''
+    fill_in 'accessory[description]', with: ''
+    fill_in 'accessory[price]', with: price
+    fill_in 'accessory[image]', with: image
+    click_on 'Create Accessory'
+
+    expect(current_path).to eq(admin_accessories_path)
+  end
+  
 end
