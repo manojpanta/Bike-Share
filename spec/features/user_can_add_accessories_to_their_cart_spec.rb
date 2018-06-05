565c05bfb81c5b6b4bffeae042165488e363128d
Manoj Panta <manojpanta95@gmail.com>
2018-06-05 16:04:54 -0600

    tested for flash message when adding accessory to the car

--- spec/features/user_can_add_accessories_to_their_cart_spec.rb
+++ spec/features/user_can_add_accessories_to_their_cart_spec.rb
@@ -32,29 +32,33 @@ describe 'as a visitor' do
         accessory = Accessory.create!(title: 'wranch', image: 'http://blog.zealousgood.com/wp-content/uploads/2013/05/tools.jpg', price: 100, description: 'this is tool')
         visit '/bike-shop'
 
-        expect(page).to have_content("Cart: 0")
+        within('.w3-badge-w3-red') do
+          expect(page).to have_content("0")
+        end
 
         click_button "Add to cart"
 
-        expect(page).to have_content("Cart: 1")
+        within('.w3-badge-w3-red') do
+          expect(page).to have_content("1")
+        end
       end
     end
   end
 end
 
-# describe 'as a visitor' do
-#   describe 'when I visit /bike-shop' do
-#     describe 'when I click on Add to cart' do
-#       it 'adds a accessory to the cart' do
-#         accessory = Accessory.create!(title: 'wranch', image: 'http://blog.zealousgood.com/wp-content/uploads/2013/05/tools.jpg', price: 100, description: 'this is tool')
+describe 'as a visitor' do
+  describe 'when I visit /bike-shop' do
+    describe 'when I click on Add to cart' do
+      it 'adds a accessory to the cart' do
+        accessory = Accessory.create!(title: 'wranch', image: 'http://blog.zealousgood.com/wp-content/uploads/2013/05/tools.jpg', price: 100, description: 'this is tool')
 
-#         visit bike_shop_path
+        visit bike_shop_path
 
-#         click_on 'Add to cart'
+        click_on 'Add to cart'
 
-#         expect(current_path).to eq(bike_shop_path)
-#         expect(page).to have_content("You now have 1 #{accessory.title} in your cart.")
-#       end
-#     end
-#   end
-# end
+        expect(current_path).to eq(bike_shop_path)
+        expect(page).to have_content("You now have 1 #{accessory.title} in your cart.")
+      end
+    end
+  end
+end
